3b28ce50e6dbb3ce94e448dfdeb09e416486d8b4
Saturn-98 <ishaan.3098@gmail.com>
2025-07-07 21:12:26 +0530

    elk stack for central logging

--- api-gateway/src/main/java/com/saturn/ecommerce/api_gateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/saturn/ecommerce/api_gateway/config/AppConfig.java
@@ -1,30 +1,17 @@
-package com.saturn.ecommerce.inventory_service.config;
+package com.saturn.ecommerce.api_gateway.config;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public ModelMapper mapper(){
-        return new ModelMapper();
-    }
-
-    @Bean
-    public RestClient restClient(){
-        return RestClient.builder().build();
-    }
-
-    @Bean
     public Capability capability(final MeterRegistry registry) {
         return new MicrometerCapability(registry);
     }
-
+    //
 }
